5d5097ee4066afc958e8a4d8095d2ecea02211c4
Ton4ee <166876423+Ton4ee@users.noreply.github.com>
2024-04-14 13:52:51 +0200

    test

--- SILab1_2.java
+++ SILab1_2.java
@@ -5,7 +5,11 @@ public class SILab1_2 {
     private static String makeBigStringFrom (List<String> list, int minLength) {
         StringBuilder sb = new StringBuilder();
         for (String el : list) {
-            if (el.length() >= minLength) {
+<<<<<<< HEAD
+            if (el.length() > minLength) {
+=======
+            if (el.length() < minLength) {
+>>>>>>> bug1
                 sb.append(el);
             }
         }
